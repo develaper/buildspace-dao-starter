f229851d2caac75d48b10bf4f3d8a4bc0791a605
develaper <ibrahim.lachguer@gmail.com>
2022-09-25 18:43:04 +0200

    Add 'Connect to Wallet' to your DAO Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { userAddress, useMetamask, useAddress } from '@thirdweb-dev/react'
+
 const App = () => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("Address:", address);
+  if(!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to MyDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          connect your wallet
+        </button>
+      </div>
+    )
+  }
+
   return (
     <div className="landing">
       <h1>Welcome to My DAO</h1>
+      <h2>wallet connected, now wat?</h2>
     </div>
   );
 };
