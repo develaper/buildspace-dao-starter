6dd8e0dfee20eeda023a58d9913e80356deaad18
develaper <ibrahim.lachguer@gmail.com>
2022-09-26 17:22:40 +0200

    Deploy your NFT bundle

---
+++ scripts/2-deploy-drop.js
@@ -0,0 +1,29 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk.js";
+import { readFileSync } from "fs";
+
+(async () => {
+  try {
+    const editionDropAddress = await sdk.deployer.deployEditionDrop({
+      name: "MyDAO Membership",
+      description: "A DAO of mine",
+      image: readFileSync("scripts/assets/mydao.jpg"),
+      primary_sale_recipient: AddressZero,
+    });
+
+    // this initialization returns the address of our contract
+    // we use this to initialize the contract on the thirdweb sdk
+    const editionDrop = sdk.getEditionDrop(editionDropAddress);
+
+    // with this, we can get the metadata of our contract
+    const metadata = await editionDrop.metadata.get();
+
+    console.log(
+      "✅ Successfully deployed editionDrop contract, address:",
+      editionDropAddress,
+    );
+    console.log("✅ editionDrop metadata:", metadata);
+  } catch (error) {
+    console.log("failed to deploy editionDrop contract", error);
+  }
+})();
